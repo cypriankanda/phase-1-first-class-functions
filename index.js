c679788b2bd756827581121afc6cc004fd8301ea
cypriankanda <cyprianboit@gmail.com>
2025-03-07 02:31:03 +0300

    first class fn push

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Function that receives a callback function and calls it
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  // 2. Function that returns a named function
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      console.log("I am a named function!");
+    };
+  }
+  
+  // 3. Function that returns an anonymous function
+  function returnsAnAnonymousFunction() {
+    return function () {
+      console.log("I am an anonymous function!");
+    };
+  }
+  
